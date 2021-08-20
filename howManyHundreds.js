bf171b7b031000e032d4a7a67f72a4b7d22c84a0
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-08-20 21:27:39 +0000

    finish howManyHundreds challenge

---
+++ howManyHundreds.js
@@ -0,0 +1,12 @@
+
+const howManyHundreds = function(num){
+  return Math.floor((num / 100));
+}
+
+console.log(101%100);
+console.log(1000%100);
+console.log(999%100);
+console.log(101/100);
+console.log(1000/100);
+console.log(999/100);
+console.log(Math.floor(999/100));
